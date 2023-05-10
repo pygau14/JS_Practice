2e40aa0bb62cc1c7a671526b8649c363820d979a
pygau14 <gaugav14@gmail.com>
2023-05-10 06:43:51 -0400

    Updated fetech.js and main.html

--- fetch.js
+++ fetch.js
@@ -20,15 +20,53 @@
 //     },
 //     "manufacturer": "OHara-Group"
 //   }
+// USE 
+// when we data in frontend - we use fetch - built in functions - handle API's 
+// API's - URL - which points to the data in backend.
 
-function show(){
-    fetch('https://api.npoint.io/aee14bc5a3f1e5ac2c7d').then((res)=>{
-        return res.json(); // json is universal format - applicable to any programming language as we are using js so to make it compatabile with js.
-    }).then((data)=>{
-        console.log(data);
+
+// fetch(URL, function ())
+
+// fetch another syntax - 
+// fetch(URL).then(()=>{
+    //fucntion when promise is ful filled || data retrive 
+// }).catch(()=>{
+    // function when promise is not fulfilled || data is not there
+// })
+
+// async -await -> fetch krke , it will retrieve , it will take time so we want code to wait for sometime.
+
+
+
+ //your task -> // DOm manipulation - name , imageurl -> 
+
+ // fetch is promise in js - 
+ // promise fulfill - complete - data retrieve kr lega 
+ // promise will not full fill - error - data 
+
+ async function show(){
+    
+    await fetch(' https://api.npoint.io/aee14bc5a3f1e5ac2c7d').then((res)=>{//res is an argument so when fetch will retrieve the data , it will automatically store the data in this varible named res.
+        return res.json(); // json is an inbuilt function to make the response as json compatible for  Javascript,
+    }).then((data)=>{ 
+        console.log('second then block') // this 'then' block is used so that I can print the data received from backend.
+         // data variable is used for storing the output of ist then block.
+
+        addList(data);
     }).catch((err)=>{
         console.log(err);
     })
-}
+ }
 
- your task -> // DOm manipulation - name , imageurl -> 
+ function addList(data){
+    const parentDiv  = document.getElementById('product_div');
+
+    for(let i=0;i<data.length;i++){
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class','div_list');
+        newDiv.innerText = data[i].name;
+        parentDiv.appendChild(newDiv);
+    }
+ }
+
+
